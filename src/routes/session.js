508a8ab28c2534d822544d88b31e473252f318ec
Ryan Floyd <mrryanfloyd@github.com>
2019-12-29 16:36:09 -0500

    Redux tutorial

--- src/routes/session.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-router.get('/', (req, res) => {
-    return res.send(req.context.models.users[req.context.me.id]);
-  });
-  export default router;
